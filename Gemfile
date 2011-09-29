99c264a293a2d27be66eeb1b775cc396846fac36
Wissam Masri <masri.wissam@gmail.com>
2011-09-29 11:19:41 +0200

    first commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.10'
+
+gem 'rails', '3.0.10'
+gem 'mysql'
+gem 'net-ssh'
+gem 'devise'
+gem 'jquery-rails'
+gem 'nokogiri'
+
+
+
+group :development do
+  gem 'rspec-rails', '2.6.1'
+end
+
+group :test do
+  gem 'rspec-rails', '2.6.1'
+  gem 'webrat', '0.7.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JuniperMSDPCInterface::Application.config.secret_token = '43ddf4da32a8f3724bfad061739c6f93f63d5f228f2d0d79cf2ca1c3c3822672ccd0edb7801f559a0a901fd8072251443436fd81fd0fad6d51b7e7625aad9073'
